7287981ecb402ffef34a21e46b461f81b90b0ca9
Kali <kali@kali.home>
2021-02-10 22:22:49 +0400

    v1.1

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
 from math import sqrt
 
-def goToSpace(toSpaced, nb):
+"""
+RULES :
+The function will return the correct conversion if the supplied problems are properly formatted, otherwise, it will **return** a **string** 
+that describes an error that is meaningful to the user.  
+* Situations that will return an error:
+    ✅ * If there are **too many problems** supplied to the function. The limit is **five**, anything more will return:
+    `Error: Too many problems.`
+    ✅ * The appropriate operators the function will accept are **addition** and **subtraction**. 
+    Multiplication and division will return an error. Other operators not mentioned in this bullet point will not need to be tested. 
+    The error returned will be:
+    `Error: Operator must be '+' or '-'.` 
+    ❌ * Each number (operand) should only contain digits. Otherwise, the function will return:
+    `Error: Numbers must only contain digits.`
+    ✅ * Each operand (aka number on each side of the operator) has a max of four digits in width. Otherwise, the error string returned will be:
+    `Error: Numbers cannot be more than four digits.`
+*  If the user supplied the correct format of problems, the conversion you return will follow these rules:
+    ✅ * There should be a single space between the operator and the longest of the two operands, the operator will be on the same line 
+    as the second operand, both operands will be in the same order as provided (the first will be the top one and the second will be the bottom.
+    ✅ * Numbers should be right-aligned.
+    ✅ * There should be four spaces between each problem.
+    ✅ * There should be dashes at the bottom of each problem. The dashes should run along the entire length of each problem individually. 
+    (The example above shows what this should look like.)
+"""
+
+def goToSpace(toSpaced, nb, sign):
     nbSpaces = int(sqrt((len(toSpaced[0])-len(toSpaced[1]))**2))
-    strSpaces = ""
-    for _ in range(nbSpaces):
-        strSpaces+=" "
     if nb:
+        strSpaces = ""
+        for _ in range(nbSpaces):
+            strSpaces+=" "
         toSpaced[0] = strSpaces+toSpaced[0]
         return toSpaced[0]
     else:
+        strSpaces = "+" if sign == "+" else "-"
+        for _ in range(nbSpaces-1):
+            strSpaces+=" "
         toSpaced[1] = strSpaces+toSpaced[1]
         return toSpaced[1]
 
@@ -40,13 +67,13 @@ def rsltFormat(answer, op):
 def casesD(op, sign, answer, answers):
     if len(op[0]) < len(op[1]):
         op[1] = "+ "+op[1] if sign == "+" else "- "+op[1]
-        op[0] = goToSpace(op, True)
+        op[0] = goToSpace(op, True, sign)
         strSep = getSep(op, False)
         answer = rsltFormat(answer, op)
         formatter = formatted(op, strSep, answer, answers)
     elif len(op[0]) > len(op[1]):
         op[0] = "  "+op[0]
-        op[1] = goToSpace(op, False)
+        op[1] = goToSpace(op, False, sign)
         strSep = getSep(op, True)
         answer = rsltFormat(answer, op)
         formatter = formatted(op, strSep, answer, answers)
@@ -58,7 +85,7 @@ def casesD(op, sign, answer, answers):
         formatter = formatted(op, strSep, answer, answers)
     return formatter
 
-def arithmetic(problems, answers=False):
+def arithmetic_arranger(problems, answers=False):
     if len(problems) > 5:
         return "Error: Too many problems."
     else:
@@ -66,15 +93,21 @@ def arithmetic(problems, answers=False):
         for pb in problems:
             if len(pb.split("+")) > 1:
                 addition = pb.split("+")
-                answer = str(int(addition[0])+int(addition[1]))
-                clc.append(casesD(addition, "+", answer, answers))
+                if len(addition[0]) <= 4 and len(addition[1]) <= 4:
+                    answer = str(int(addition[0])+int(addition[1]))
+                    clc.append(casesD(addition, "+", answer, answers))
+                elif len(addition[0]) >= 5 or len(addition[1]) >= 5:
+                    return "Error: Numbers cannot be more than four digits."
             elif len(pb.split("-")) > 1:
                 substraction = pb.split("-")
-                answer = str(int(substraction[0])-int(substraction[1]))
-                clc.append(casesD(substraction, "-", answer, answers))
+                if len(substraction[0]) <= 4 and len(substraction[1]) <= 4:
+                    answer = str(int(substraction[0])-int(substraction[1]))
+                    clc.append(casesD(substraction, "-", answer, answers))
+                elif len(substraction[0]) >= 5 or len(substraction[1]) >= 5:
+                    return "Error: Numbers cannot be more than four digits."
             elif  len(pb.split("/")) > 1 or len(pb.split("*")) > 1:
                 return "Error: Operator must be '+' or '-'."
-        
+
         allClcStr = ""
         top = []
         bot = []
@@ -101,4 +134,4 @@ def arithmetic(problems, answers=False):
             allClcStr+=r
         return allClcStr
 
-print(arithmetic(["2000-20000", "20+2000", "2000-2000", "2000+20", "2000-2000"], True))
+print(arithmetic_arranger(["2000-2000", "2000+2000", "20-2000", "20+2000", "2000-2000"], True))
