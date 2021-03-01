a603e7c4792ed21969f9e529982239602184ad39
Leocadie <leocadiepierre@gmail.com>
2021-03-01 18:24:59 +0400

    v1.4

--- python/projectOne/main.py
+++ python/projectOne/main.py
@@ -1,30 +1,6 @@
 from math import sqrt
 import re
 
-"""
-RULES :
-The function will return the correct conversion if the supplied problems are properly formatted, otherwise, it will **return** a **string** 
-that describes an error that is meaningful to the user.  
-* Situations that will return an error:
-    ✅ * If there are **too many problems** supplied to the function. The limit is **five**, anything more will return:
-    `Error: Too many problems.`
-    ✅ * The appropriate operators the function will accept are **addition** and **subtraction**. 
-    Multiplication and division will return an error. Other operators not mentioned in this bullet point will not need to be tested. 
-    The error returned will be:
-    `Error: Operator must be '+' or '-'.` 
-    ✅ * Each number (operand) should only contain digits. Otherwise, the function will return:
-    `Error: Numbers must only contain digits.`
-    ✅ * Each operand (aka number on each side of the operator) has a max of four digits in width. Otherwise, the error string returned will be:
-    `Error: Numbers cannot be more than four digits.`
-*  If the user supplied the correct format of problems, the conversion you return will follow these rules:
-    ✅ * There should be a single space between the operator and the longest of the two operands, the operator will be on the same line 
-    as the second operand, both operands will be in the same order as provided (the first will be the top one and the second will be the bottom.
-    ✅ * Numbers should be right-aligned.
-    ✅ * There should be four spaces between each problem.
-    ✅ * There should be dashes at the bottom of each problem. The dashes should run along the entire length of each problem individually. 
-    (The example above shows what this should look like.)
-"""
-
 def goToSpace(toSpaced, nb, sign):
     nbSpaces = int(sqrt((len(toSpaced[0])-len(toSpaced[1]))**2))
     if nb:
@@ -119,11 +95,7 @@ def arithmetic_arranger(problems, answers=False):
                     return "Error: Operator must be '+' or '-'."
             else:
                 return "Error: Numbers must only contain digits."
-        allClcStr = ""
-        top = []
-        bot = []
-        sep = []
-        rslt = []
+        top, bot, sep, rslt = [], [], [], []
         btwClc = "    "
         for c in clc:
             csplit = c.split("\n")
@@ -135,14 +107,16 @@ def arithmetic_arranger(problems, answers=False):
         bot[-1]+="\n"
         sep[-1]+="\n"
         rslt[-1]+="\n"
+        tp, bt, sp, rt = "", "", "", ""
         for t in top:
-            allClcStr+=t
+            tp+=t
         for b in bot:
-            allClcStr+=b
+            bt+=b
         for s in sep:
-            allClcStr+=s
+            sp+=s
         for r in rslt:
-            allClcStr+=r
-        return allClcStr
+            rt+=r
+        arranged_problems = tp+bt+sp+rt
+        return arranged_problems
 
-print(arithmetic_arranger(["2000-200", "2000 + 2000", "20-2000", "20+2000", "2000 + 20"], True))
+#print(arithmetic_arranger(["2000-200", "2000 + 2000", "20-2000", "20+2000", "2000 + 20"], True))
