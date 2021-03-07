7cb17d539b9387e325c6e234b30962d7421fd1b8
Leocadie <leocadiepierre@gmail.com>
2021-03-07 20:47:36 +0400

    v2.0.0

--- python/projectTwo/main.py
+++ python/projectTwo/main.py
@@ -19,15 +19,18 @@ def add_time(start_time, duration, starting_day=""):
     else:
         return "Error, minutes must be less than 60"
     
+    nbDays = 0
     if ending.lower() == "am":
-        nbDays = 1
         if rsltM < 60:
-            if rsltH <= 12:
+            if rsltH < 12:
                 ending = "am"
+            elif rsltH == 12:
+                ending = "pm"
             elif rsltH > 12:
+                if rsltH > 24:
+                    nbDays = int(round(float(rsltH)/24.0))
                 while rsltH > 24:
                     rsltH-=24
-                    nbDays+=1
                 if rsltH <= 12:
                     ending = "am"
                 elif rsltH > 12:
@@ -36,84 +39,106 @@ def add_time(start_time, duration, starting_day=""):
         if rsltM > 59:
             rsltM-=60
             rsltH+=1
-            if rsltH <= 12:
+            if rsltH < 12:
                 ending = "am"
-            elif rsltH > 12:
-                while rsltH > 24:
-                    rsltH-=24
-                    nbDays+=1
-                if rsltH <= 12:
-                    ending = "am"
-                elif rsltH > 12:
-                    rsltH-=12
-                    ending = "pm"
-        rsltH = "0"+str(rsltH) if len(str(rsltH)) < 2 else str(rsltH)
-        rsltM = "0"+str(rsltM) if len(str(rsltM)) < 2 else str(rsltM)
-        nxtD = " (next day)" if nbDays == 1 else ""
-        new_time = rsltH+":"+str(rsltM)+" "+ending.upper()+nxtD
-    elif ending.lower() == "pm":
-        nbDays = 1
-        if rsltM < 60:
-            if rsltH <= 12:
+            elif rsltH == 12:
                 ending = "pm"
             elif rsltH > 12:
+                if rsltH > 24:
+                    nbDays = int(round(float(rsltH)/24.0))
                 while rsltH > 24:
                     rsltH-=24
-                    nbDays+=1
                 if rsltH <= 12:
+                    ending = "am"
+                elif rsltH > 12:
+                    rsltH-=12
                     ending = "pm"
+        rsltM = "0"+str(rsltM) if len(str(rsltM)) < 2 else str(rsltM)
+        new_time = str(rsltH)+":"+str(rsltM)+" "+ending.upper()
+    elif ending.lower() == "pm":
+        if rsltM < 60:
+            if rsltH < 12:
+                ending = "pm"
+            elif rsltH == 12:
+                ending = "am"
+            elif rsltH > 12:
+                if rsltH > 24:
+                    nbDays = int(round(float(rsltH)/24.0))
+                while rsltH > 24:
+                    rsltH-=24
+                if rsltH < 12:
+                    ending = "pm"
+                elif rsltH == 12:
+                    ending = "am"
                 elif rsltH > 12:
                     rsltH-=12
                     ending = "am"
+                    if nbDays == 0:
+                        nbDays = 1
         elif rsltM > 59:
             rsltM-=60
             rsltH+=1
-            if rsltH <= 12:
+            if rsltH < 12:
                 ending = "pm"
+            elif rsltH == 12:
+                ending = "am"
             elif rsltH > 12:
+                if rsltH > 24:
+                    nbDays = int(round(float(rsltH)/24.0))
                 while rsltH > 24:
                     rsltH-=24
-                    nbDays+=1
-                if rsltH <= 12:
+                if rsltH < 12:
                     ending = "pm"
+                elif rsltH == 12:
+                    ending = "am"
                 elif rsltH > 12:
                     rsltH-=12
                     ending = "am"
-        rsltH = "0"+str(rsltH) if len(str(rsltH)) < 2 else str(rsltH)
+                    if nbDays == 0:
+                        nbDays = 1
         rsltM = "0"+str(rsltM) if len(str(rsltM)) < 2 else str(rsltM)
-        nxtD = " (next day)" if nbDays == 1 else ""
-        new_time = rsltH+":"+str(rsltM)+" "+ending.upper()+nxtD
+        new_time = str(rsltH)+":"+str(rsltM)+" "+ending.upper()
     else:
         return "Error, ending must be AM or PM."
     
     if starting_day:
         starting_day = starting_day.lower().capitalize()
         if starting_day in days:
-            if not nxtD:
-                if nbDays == 0:
-                    starting_day = ", "+starting_day
-                    new_time+=starting_day
-                elif nbDays == 2:
-                    strtD_index = days.index(starting_day)
-                    nD_index = nbDays+strtD_index
-                    if nD_index > 6:
-                        nD_index-=7
-                    tD_later = ", "+days[nD_index]+" "+"(2 days later)"
-                    new_time+=tD_later
-                elif nbDays > 2:
-                    strtD_index = days.index(starting_day)
-                    nD_index = nbDays+strtD_index
-                    while nD_index > 6:
-                        nD_index-=7
-                    xD_later = ", "+days[nD_index]+" ("+str(nbDays)+" days later)"
-                    new_time+=xD_later
+            if nbDays == 0:
+                starting_day = ", "+starting_day
+                new_time+=starting_day
+            elif nbDays == 1:
+                strtD_index = days.index(starting_day)
+                nD_index = nbDays+strtD_index
+                if nD_index > 6:
+                    nD_index-=7
+                nxtDay = ", "+days[nD_index]+" (next day)"
+                new_time+=nxtDay
+            elif nbDays == 2:
+                strtD_index = days.index(starting_day)
+                nD_index = nbDays+strtD_index
+                if nD_index > 6:
+                    nD_index-=7
+                tD_later = ", "+days[nD_index]+" "+"(2 days later)"
+                new_time+=tD_later
+            elif nbDays > 2:
+                strtD_index = days.index(starting_day)
+                nD_index = nbDays+strtD_index
+                while nD_index > 6:
+                    nD_index-=7
+                xD_later = ", "+days[nD_index]+" ("+str(nbDays)+" days later)"
+                new_time+=xD_later
         else:
             return "Error, your starting day isn't a valid day."
     elif not starting_day:
-        if nbDays > 2:
+        if nbDays == 1:
+            new_time+=" (next day)"
+        elif nbDays == 2:
+            new_time+=" (2 days later)"
+        elif nbDays > 2:
             xD_later = " ("+str(nbDays)+" days later)"
             new_time+=xD_later
     
     return new_time
 
-print(add_time("11:55 AM", "3:12"))
+print(add_time("11:59 PM", "24:05"))
